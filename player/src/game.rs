a2c6e1e1cdc5bed037b1638ee3df73ba3446334e
Ryan1729 <Ryan1729@gmail.com>
2018-04-08 22:41:24 -0600

    demo of overwriting game.rs and reorganize
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

---
+++ player/src/game.rs
@@ -0,0 +1,58 @@
+use common::*;
+
+impl GameState {
+    fn isAvatar(&self, id: usize) -> bool {
+        self.entities[id].contains(Component::Player) && self.player_types[id] == PlayerType::Avatar
+    }
+}
+
+#[inline]
+pub fn update_and_render(state: &mut GameState, framebuffer: &mut Framebuffer, input: Input) {
+    for i in 0..GameState::ENTITY_COUNT {
+        if state.mode == Mode::MoveAvatar && state.isAvatar(i) {
+            let appearance = &mut state.appearances[i];
+
+            if appearance.is_offset() {
+                appearance.reduce_offset(8);
+                continue;
+            }
+
+            let (mut x, mut y) = state.positions[i];
+
+            if input.pressed_this_frame(Button::Left) && x > 0 {
+                x = x.saturating_sub(1);
+                appearance.offset.0 = CELL_WIDTH as isize;
+            }
+
+            if input.pressed_this_frame(Button::Right) && x < BOARD_WIDTH - 1 {
+                x = x.saturating_add(1);
+                appearance.offset.0 = -(CELL_WIDTH as isize)
+            }
+
+            if input.pressed_this_frame(Button::Up) && y > 0 {
+                y = y.saturating_sub(1);
+                appearance.offset.1 = CELL_WIDTH as isize;
+            }
+
+            if input.pressed_this_frame(Button::Down) && y < BOARD_HEIGHT - 1 {
+                y = y.saturating_add(1);
+                appearance.offset.1 = -(CELL_WIDTH as isize);
+            }
+
+            state.positions[i] = (x, y);
+        }
+    }
+
+    framebuffer.clear();
+
+    for i in 0..GameState::ENTITY_COUNT {
+        let entity = state.entities[i];
+        if entity.contains(Component::Position | Component::Appearance) {
+            let pos = state.positions[i];
+
+            let appearance = &mut state.appearances[i];
+            appearance.render_positioned(framebuffer, pos);
+        }
+    }
+}
+    
