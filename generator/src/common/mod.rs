e68d8d1eb63b36dc1125876eae913b22b2e1f260
Ryan1729 <Ryan1729@gmail.com>
2018-06-03 20:59:13 -0600

    move `Distribution` impls closer to definition

--- generator/src/common/mod.rs
+++ generator/src/common/mod.rs
@@ -1,5 +1,9 @@
 use std::fmt;
 
+use rand::{
+    distributions::{Distribution, Standard}, Rng,
+};
+
 pub use project_common::*;
 
 macro_rules! code_string {
@@ -32,98 +36,6 @@ pub enum DeckType {
     ThreeColour(u8),
 }
 pub use DeckType::*;
-
-#[derive(Debug)]
-pub struct GridGameSpec {
-    pub grid_dimensions: (u8, u8),
-    pub goal: Goal,
-    pub entity_animacies: Vec<EntityAnimacy>,
-    pub entity_controls: Vec<Option<EntityControl>>,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub enum Goal {
-    AddSomeNumberOfXsToTheGrid,
-    MoveAllXsToALocation,
-    FreeAllTrappedXs,
-    TransformAllXsIntoYs,
-    MakeAllXsTouchTheGroupofXs,
-    MakeAllXsTouchAtLeastOneY,
-}
-pub use Goal::*;
-
-pub const GOAL_COUNT: u8 = 4;
-
-impl Goal {
-    pub fn minimum_entity_types_needed(&self) -> u8 {
-        //We always need at least one player controlled entity
-        match *self {
-            AddSomeNumberOfXsToTheGrid => 1,
-            MoveAllXsToALocation => 1,
-            FreeAllTrappedXs => 1,
-            //X _cannot_ be the same as Y
-            TransformAllXsIntoYs => 2,
-            MakeAllXsTouchTheGroupofXs => 1,
-            //X _can_ be the same as Y
-            MakeAllXsTouchAtLeastOneY => 2,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum EntityAnimacy {
-    PlayerControlled,
-    Inanimate,
-    Animate,
-}
-pub use EntityAnimacy::*;
-
-impl Default for EntityAnimacy {
-    fn default() -> EntityAnimacy {
-        PlayerControlled
-    }
-}
-
-impl fmt::Display for EntityAnimacy {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let result = match *self {
-            PlayerControlled => "Component::Player",
-            Inanimate => "Component::Ty::empty()",
-            Animate => "Component::Animate",
-        };
-
-        write!(f, "{}", result,)?;
-
-        Ok(())
-    }
-}
-
-pub const ENTITY_ANIMACY_COUNT: u8 = 3;
-
-#[derive(Copy, Clone, Debug)]
-pub struct EntityControl {
-    pub movement: MoveControl,
-    pub a: Action,
-    pub b: Action,
-    pub select: Action,
-}
-
-#[derive(Copy, Clone, Debug)]
-pub enum MoveControl {
-    Orthogonal,
-    Diagonal,
-}
-pub use MoveControl::*;
-
-pub const MOVE_CONTROL_COUNT: u8 = 2;
-
-//Moreso than any other game element, this set of possibilities will need to be expaneded
-#[derive(Copy, Clone, Debug)]
-pub enum Action {
-    SwapPlayerControlToNext(u8),
-    CopySelf,
-}
-pub use Action::*;
 
 #[derive(Default)]
 pub struct ButtonResponses {
@@ -280,4 +192,213 @@ impl fmt::Display for InputResponder {
 
         Ok(())
     }
+}
+
+impl Distribution<GridGameSpec> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> GridGameSpec {
+        let grid_dimensions = (
+            rng.gen_range(0, SCREEN_WIDTH) as u8,
+            rng.gen_range(0, SCREEN_HEIGHT) as u8,
+        );
+
+        let goal = rng.gen();
+
+        let entity_animacies = generate_entity_animacies(rng, goal);
+
+        let entity_controls = generate_entity_controls(rng, &entity_animacies);
+
+        GridGameSpec {
+            grid_dimensions,
+            goal,
+            entity_animacies,
+            entity_controls,
+        }
+    }
+}
+
+impl Distribution<Goal> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Goal {
+        match rng.gen_range(0, GOAL_COUNT) {
+            0 => AddSomeNumberOfXsToTheGrid,
+            1 => MoveAllXsToALocation,
+            2 => FreeAllTrappedXs,
+            3 => TransformAllXsIntoYs,
+            4 => MakeAllXsTouchTheGroupofXs,
+            _ => MakeAllXsTouchAtLeastOneY,
+        }
+    }
+}
+
+fn generate_entity_animacies<R: Rng + ?Sized>(rng: &mut R, goal: Goal) -> Vec<EntityAnimacy> {
+    let minimum_entity_types_needed = goal.minimum_entity_types_needed();
+
+    let entity_animacies_len =
+        rng.gen_range(minimum_entity_types_needed, minimum_entity_types_needed * 2);
+
+    debug_assert!(entity_animacies_len >= 1);
+
+    let mut entity_animacies = Vec::with_capacity(entity_animacies_len as _);
+
+    entity_animacies.push(PlayerControlled);
+
+    for _ in 1..entity_animacies_len {
+        entity_animacies.push(rng.gen());
+    }
+
+    entity_animacies
+}
+
+impl Distribution<EntityAnimacy> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> EntityAnimacy {
+        match rng.gen_range(0, ENTITY_ANIMACY_COUNT) {
+            0 => PlayerControlled,
+            1 => Inanimate,
+            _ => Animate,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct GridGameSpec {
+    pub grid_dimensions: (u8, u8),
+    pub goal: Goal,
+    pub entity_animacies: Vec<EntityAnimacy>,
+    pub entity_controls: Vec<Option<EntityControl>>,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum Goal {
+    AddSomeNumberOfXsToTheGrid,
+    MoveAllXsToALocation,
+    FreeAllTrappedXs,
+    TransformAllXsIntoYs,
+    MakeAllXsTouchTheGroupofXs,
+    MakeAllXsTouchAtLeastOneY,
+}
+pub use Goal::*;
+
+pub const GOAL_COUNT: u8 = 4;
+
+impl Goal {
+    pub fn minimum_entity_types_needed(&self) -> u8 {
+        //We always need at least one player controlled entity
+        match *self {
+            AddSomeNumberOfXsToTheGrid => 1,
+            MoveAllXsToALocation => 1,
+            FreeAllTrappedXs => 1,
+            //X _cannot_ be the same as Y
+            TransformAllXsIntoYs => 2,
+            MakeAllXsTouchTheGroupofXs => 1,
+            //X _can_ be the same as Y
+            MakeAllXsTouchAtLeastOneY => 2,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum EntityAnimacy {
+    PlayerControlled,
+    Inanimate,
+    Animate,
+}
+pub use EntityAnimacy::*;
+
+pub const ENTITY_ANIMACY_COUNT: u8 = 3;
+
+impl Distribution<EntityControl> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> EntityControl {
+        EntityControl {
+            movement: rng.gen(),
+            a: rng.gen(),
+            b: rng.gen(),
+            select: rng.gen(),
+        }
+    }
+}
+
+impl Default for EntityAnimacy {
+    fn default() -> EntityAnimacy {
+        PlayerControlled
+    }
+}
+
+impl fmt::Display for EntityAnimacy {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let result = match *self {
+            PlayerControlled => "Component::Player",
+            Inanimate => "Component::Ty::empty()",
+            Animate => "Component::Animate",
+        };
+
+        write!(f, "{}", result,)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct EntityControl {
+    pub movement: MoveControl,
+    pub a: Action,
+    pub b: Action,
+    pub select: Action,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum MoveControl {
+    Orthogonal,
+    Diagonal,
+}
+pub use MoveControl::*;
+
+pub const MOVE_CONTROL_COUNT: u8 = 2;
+
+impl Distribution<MoveControl> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> MoveControl {
+        match rng.gen_range(0, MOVE_CONTROL_COUNT) {
+            0 => Orthogonal,
+            _ => Diagonal,
+        }
+    }
+}
+
+//Moreso than any other game element, this set of possibilities will need to be expaneded
+#[derive(Copy, Clone, Debug)]
+pub enum Action {
+    SwapPlayerControlToNext(u8),
+    CopySelf,
+}
+pub use Action::*;
+
+impl Distribution<Action> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Action {
+        match rng.gen_range(0, MOVE_CONTROL_COUNT) {
+            0 => SwapPlayerControlToNext(rng.gen()),
+            _ => CopySelf,
+        }
+    }
+}
+
+fn generate_entity_controls<R: Rng + ?Sized>(
+    rng: &mut R,
+    entity_animacies: &Vec<EntityAnimacy>,
+) -> Vec<Option<EntityControl>> {
+    let controls_len = entity_animacies.len();
+
+    debug_assert!(controls_len >= 1);
+
+    let mut controls = Vec::with_capacity(controls_len);
+
+    for e in entity_animacies.iter() {
+        let contol = match *e {
+            Inanimate => None,
+            //I could try to encode this relationship between Animacy and Controls in the type by
+            //merging those two types, but that smells like unnecessary coupling.
+            _ => Some(rng.gen()),
+        };
+
+        controls.push(contol);
+    }
+
+    controls
 }
